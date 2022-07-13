1b3ceb21623c8bc146435150ed72c62c31646d16
Jorge Flores <flores.jorgeluis@gmail.com>
2022-07-13 16:26:49 -0500

    regla adicional

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
 // 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
 //    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
 
+// Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
+// De igual manera al llegar los valores a 0 no se podrá decrementas más.
+
   return (
     <div className="app-container">
       <div className="temperature-display-container">
